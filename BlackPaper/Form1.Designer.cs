50d9cf845535a3c477a3c2d7b7269ae5fb3f6987
hontsev <hontsev@gmail.com>
2016-09-10 10:25:35 +0800

    files

---
+++ BlackPaper/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace BlackPaper
+{
+    partial class BlackForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BlackForm));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.黑度1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.透明ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.自动调整亮度ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.开机自动启动ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.关闭菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "BlackPaper";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem1,
+            this.黑度1ToolStripMenuItem,
+            this.透明ToolStripMenuItem,
+            this.自动调整亮度ToolStripMenuItem,
+            this.开机自动启动ToolStripMenuItem,
+            this.退出ToolStripMenuItem,
+            this.关闭菜单ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 246);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem4.Text = "最黑+++++";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click_1);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem3.Text = "极黑++++";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click_1);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem2.Text = "更黑+++";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click_1);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem1.Text = "较黑++";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // 黑度1ToolStripMenuItem
+            // 
+            this.黑度1ToolStripMenuItem.Name = "黑度1ToolStripMenuItem";
+            this.黑度1ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.黑度1ToolStripMenuItem.Text = "稍黑+";
+            this.黑度1ToolStripMenuItem.Click += new System.EventHandler(this.黑度1ToolStripMenuItem_Click);
+            // 
+            // 透明ToolStripMenuItem
+            // 
+            this.透明ToolStripMenuItem.Name = "透明ToolStripMenuItem";
+            this.透明ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.透明ToolStripMenuItem.Text = "透明 -";
+            this.透明ToolStripMenuItem.Click += new System.EventHandler(this.透明ToolStripMenuItem_Click);
+            // 
+            // 自动调整亮度ToolStripMenuItem
+            // 
+            this.自动调整亮度ToolStripMenuItem.Name = "自动调整亮度ToolStripMenuItem";
+            this.自动调整亮度ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.自动调整亮度ToolStripMenuItem.Text = "自动亮度 @";
+            this.自动调整亮度ToolStripMenuItem.Click += new System.EventHandler(this.自动调整亮度ToolStripMenuItem_Click);
+            // 
+            // 开机自动启动ToolStripMenuItem
+            // 
+            this.开机自动启动ToolStripMenuItem.Name = "开机自动启动ToolStripMenuItem";
+            this.开机自动启动ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.开机自动启动ToolStripMenuItem.Text = "开机自动启动";
+            this.开机自动启动ToolStripMenuItem.Click += new System.EventHandler(this.开机自动启动ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 300000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // 关闭菜单ToolStripMenuItem
+            // 
+            this.关闭菜单ToolStripMenuItem.Name = "关闭菜单ToolStripMenuItem";
+            this.关闭菜单ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.关闭菜单ToolStripMenuItem.Text = "关闭菜单";
+            this.关闭菜单ToolStripMenuItem.Click += new System.EventHandler(this.关闭菜单ToolStripMenuItem_Click_1);
+            // 
+            // BlackForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(917, 365);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BlackForm";
+            this.Opacity = 0.5D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "BlackPaper";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 黑度1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 透明ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 开机自动启动ToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem 自动调整亮度ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关闭菜单ToolStripMenuItem;
+    }
+}
+
---
+++ BlackPaper/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+
+namespace BlackPaper
+{
+    public partial class BlackForm : Form
+    {
+        //是否开机自动启动
+        private bool isAutoRun = false;
+        //是否自动调整亮度，默认打开程序时为开
+        private bool isAutoChange = true;
+
+        private const uint WS_EX_LAYERED = 0x80000;
+        private const int WS_EX_TRANSPARENT = 0x20;
+        private const int GWL_STYLE = (-16);
+        private const int GWL_EXSTYLE = (-20);
+        private const int LWA_ALPHA = 0x2;
+
+        [DllImport("user32", EntryPoint = "SetWindowLong")]
+        private static extern uint SetWindowLong(
+        IntPtr hwnd,
+        int nIndex,
+        uint dwNewLong
+        );
+
+        [DllImport("user32", EntryPoint = "GetWindowLong")]
+        private static extern uint GetWindowLong(
+        IntPtr hwnd,
+        int nIndex
+        );
+
+        [DllImport("user32", EntryPoint = "SetLayeredWindowAttributes")]
+        private static extern int SetLayeredWindowAttributes(
+        IntPtr hwnd,
+        int crKey,
+        int bAlpha,
+        int dwFlags
+        );
+
+        ///<summary>
+        /// 设置窗体具有鼠标穿透效果
+        ///</summary>
+        public void SetPenetrate()
+        {
+            this.TopMost = true;
+            GetWindowLong(this.Handle, GWL_EXSTYLE);
+            SetWindowLong(this.Handle, GWL_EXSTYLE, WS_EX_TRANSPARENT | WS_EX_LAYERED);
+            SetLayeredWindowAttributes(this.Handle, 0, 100, LWA_ALPHA);
+        }
+
+        public BlackForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetPenetrate();
+            checkIfAutoRun();
+            setAutoLight();
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(MousePosition);
+            }
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void 透明ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Opacity = 0;
+            透明ToolStripMenuItem.Checked = true;
+            黑度1ToolStripMenuItem.Checked = false;
+            toolStripMenuItem1.Checked = false;
+            toolStripMenuItem2.Checked = false;
+            toolStripMenuItem3.Checked = false;
+            toolStripMenuItem4.Checked = false;
+            isAutoChange = false;
+            自动调整亮度ToolStripMenuItem.Checked = isAutoChange;
+        }
+
+        private void 黑度1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Opacity = 0.10;
+            透明ToolStripMenuItem.Checked = false;
+            黑度1ToolStripMenuItem.Checked = true;
+            toolStripMenuItem1.Checked = false;
+            toolStripMenuItem2.Checked = false;
+            toolStripMenuItem3.Checked = false;
+            toolStripMenuItem4.Checked = false;
+            isAutoChange = false;
+            自动调整亮度ToolStripMenuItem.Checked = isAutoChange;
+        }
+
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Opacity = 0.25;
+            透明ToolStripMenuItem.Checked = false;
+            黑度1ToolStripMenuItem.Checked = false;
+            toolStripMenuItem1.Checked = true;
+            toolStripMenuItem2.Checked = false;
+            toolStripMenuItem3.Checked = false;
+            toolStripMenuItem4.Checked = false;
+            isAutoChange = false;
+            自动调整亮度ToolStripMenuItem.Checked = isAutoChange;
+        }
+
+        private void toolStripMenuItem2_Click_1(object sender, EventArgs e)
+        {
+            this.Opacity = 0.45;
+            透明ToolStripMenuItem.Checked = false;
+            黑度1ToolStripMenuItem.Checked = false;
+            toolStripMenuItem1.Checked = false;
+            toolStripMenuItem2.Checked = true;
+            toolStripMenuItem3.Checked = false;
+            toolStripMenuItem4.Checked = false;
+            isAutoChange = false;
+            自动调整亮度ToolStripMenuItem.Checked = isAutoChange;
+        }
+
+        private void toolStripMenuItem3_Click_1(object sender, EventArgs e)
+        {
+            this.Opacity = 0.65;
+            透明ToolStripMenuItem.Checked = false;
+            黑度1ToolStripMenuItem.Checked = false;
+            toolStripMenuItem1.Checked = false;
+            toolStripMenuItem2.Checked = false;
+            toolStripMenuItem3.Checked = true;
+            toolStripMenuItem4.Checked = false;
+            isAutoChange = false;
+            自动调整亮度ToolStripMenuItem.Checked = isAutoChange;
+        }
+
+        private void toolStripMenuItem4_Click_1(object sender, EventArgs e)
+        {
+            this.Opacity = 0.85;
+            透明ToolStripMenuItem.Checked = false;
+            黑度1ToolStripMenuItem.Checked = false;
+            toolStripMenuItem1.Checked = false;
+            toolStripMenuItem2.Checked = false;
+            toolStripMenuItem3.Checked = false;
+            toolStripMenuItem4.Checked = true;
+            isAutoChange = false;
+            自动调整亮度ToolStripMenuItem.Checked = isAutoChange;
+        }
+
+        #region 开机自动运行相关
+
+        /// <summary>
+        /// 检查是否开机自动启动，设置按钮状态等
+        /// </summary>
+        private void checkIfAutoRun()
+        {
+            RegistryKey loca_chek = Registry.CurrentUser;
+            RegistryKey run_Check = loca_chek.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+            if (run_Check.GetValue("BlackPaper") != null && run_Check.GetValue("BlackPaper").ToString().ToLower() != "false")
+            {
+                isAutoRun = true;
+            }
+            else
+            {
+                isAutoRun = false;
+            }
+            开机自动启动ToolStripMenuItem.Checked = isAutoRun;
+        }
+
+        /// <summary>
+        /// 做开机自动启动设置，根据isAutoRun值设为或者取消开机自动启动
+        /// </summary>
+        private void AutoRun()
+        {
+            //获取程序执行路径
+            string starupPath = Application.ExecutablePath;
+            //class Micosoft.Win32.RegistryKey. 表示Window注册表中项级节点,此类是注册表装.
+            RegistryKey loca = Registry.CurrentUser;
+            RegistryKey run = loca.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+            try
+            {
+                //SetValue:存储值的名称
+                if (isAutoRun == true)
+                {
+                    run.DeleteValue("BlackPaper");
+                    //run.SetValue("BlackPaper", false);//取消开机运行
+                    isAutoRun = false;
+                }
+                else
+                {
+                    run.SetValue("BlackPaper", starupPath);//设置开机运行
+                    isAutoRun = true;
+                }
+                loca.Close();
+            }
+            catch
+            { }
+        }
+        #endregion
+
+        /// <summary>
+        /// 根据当前系统时间设置自动亮度
+        /// </summary>
+        private void setAutoLight()
+        {
+            if (isAutoChange)
+            {
+                int nowHour = DateTime.Now.Hour;
+                int nowMinute = DateTime.Now.Minute;
+                if (nowHour >= 8 && nowHour <= 18)
+                {
+                    //白天透明
+                    this.Opacity = 0;
+                }
+                else if (nowHour > 18 && nowHour <= 22)
+                {
+                    //傍晚四分之一
+                    this.Opacity = 0.25;
+                }
+                else
+                {
+                    //晚上半暗
+                    this.Opacity = 0.5;
+                }
+
+                透明ToolStripMenuItem.Checked = false;
+                黑度1ToolStripMenuItem.Checked = false;
+                toolStripMenuItem1.Checked = false;
+                toolStripMenuItem2.Checked = false;
+                toolStripMenuItem3.Checked = false;
+                toolStripMenuItem4.Checked = false;
+            }
+            else
+            {
+                this.Opacity = 0.25;
+            }
+            自动调整亮度ToolStripMenuItem.Checked = isAutoChange;
+        }
+
+        private void 开机自动启动ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AutoRun();
+            开机自动启动ToolStripMenuItem.Checked = isAutoRun;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //定时准备自动调整亮度
+            setAutoLight();
+        }
+
+        private void 自动调整亮度ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (isAutoChange)
+            {
+                isAutoChange = false;
+            }
+            else
+            {
+                isAutoChange = true;
+                setAutoLight();
+            }
+            自动调整亮度ToolStripMenuItem.Checked = isAutoChange;
+        }
+
+        private void 关闭菜单ToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            contextMenuStrip1.Hide();
+        }
+    }
+}
